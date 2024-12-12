a69341a49c9314313fc2592ebfbe6434bc9aae7f
Sayantan Karmakar <sayantankarmakar@outlook.com>
2024-12-13 02:11:27 +0530

    chore: move to upstash redis adapter for auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,21 @@
-import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { db } from "@/packages/db/drizzle";
+import NextAuth from "next-auth";
+import { UpstashRedisAdapter } from "@auth/upstash-redis-adapter";
+import { Redis } from "@upstash/redis";
+
+const ENVIRONMENT = process.env.ENVIRONMENT;
+if (ENVIRONMENT == null) {
+	throw new Error("ENVIRONMENT is not set");
+}
+
+const redis = new Redis({
+	url: process.env.UPSTASH_REDIS_URL!,
+	token: process.env.UPSTASH_REDIS_TOKEN!,
+});
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-	adapter: DrizzleAdapter(db),
+	adapter: UpstashRedisAdapter(redis, {
+		baseKeyPrefix: `liars-browser:${ENVIRONMENT}:`,
+	}),
 	providers: [Google],
 });
